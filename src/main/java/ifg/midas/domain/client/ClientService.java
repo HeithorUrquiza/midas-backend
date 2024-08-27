785fd21c680697e23ad70d61d8af5d1fe33c1fe9
Heithor Urquiza <111160912+HeithorUrquiza@users.noreply.github.com>
2024-08-27 11:53:25 -0300

    refactor: Ajustando a funcionalidade de exclusão de entidades Client associadas a entidade Group

--- src/main/java/ifg/midas/domain/client/ClientService.java
+++ src/main/java/ifg/midas/domain/client/ClientService.java
@@ -3,7 +3,11 @@ package ifg.midas.domain.client;
 import ifg.midas.domain.client.dto.ClientRegistryDTO;
 import ifg.midas.domain.client.dto.ClientUpdateDTO;
 import ifg.midas.domain.commodity.CommodityRepository;
+import ifg.midas.domain.group.Group;
+import ifg.midas.domain.group.GroupRepository;
+import ifg.midas.domain.site.Site;
 import ifg.midas.domain.site.SiteRepository;
+import ifg.midas.domain.strategy.Strategy;
 import ifg.midas.domain.strategy.StrategyRepository;
 import ifg.midas.domain.token.TokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +18,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
 
     @Transactional
     public Client registryClient(ClientRegistryDTO clientRegistryDTO) {
@@ -36,6 +43,15 @@ public class ClientService {
     @Transactional
     public void deleteClient(Long id) {
         Client clientDB = this.clientRepository.getReferenceById(id);
-        this.clientRepository.deleteById(clientDB.getId());
+//        To ensure the deletion of clients on groups in MayToMay relationship
+        for (Group group : clientDB.getGroups()) {
+            group.getMembers().remove(clientDB);
+            this.groupRepository.save(group);
+
+            if (group.getMembers().isEmpty()) {
+                this.groupRepository.delete(group);
+            }
+        }
+        this.clientRepository.delete(clientDB);
     }
 }
